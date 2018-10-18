a2eab99165981f5b48e2acb9a78031e1ad27f193
Krasimir Angelov <krasio@codingspree.net>
2018-10-19 09:16:59 +1300

    Fix - configure default public keys and recipients for non-production environments to make tests pass.

--- config/initializers/gpg.rb
+++ config/initializers/gpg.rb
@@ -1,9 +1,15 @@
-GPG_RECIPIENTS = ENV.fetch("MUNA_GPG_RECIPIENTS").split(",")
+recipients = if Rails.env.production?
+               ENV.fetch("MUNA_GPG_RECIPIENTS")
+             else
+               ENV.fetch("MUNA_GPG_RECIPIENTS", "muna.test@fluxfederation.com")
+             end
+GPG_RECIPIENTS = recipients.split(",")
 
 if public_keys = ENV.fetch("MUNA_GPG_PUBLIC_KEYS", false)
   GPGME::Key.import(public_keys)
 end
 
-if public_keys_path = ENV.fetch("MUNA_GPG_PUBLIC_KEYS_PATH", false)
+default_public_keys_path = File.join(Rails.root, "test", "support") unless Rails.env.production?
+if public_keys_path = ENV.fetch("MUNA_GPG_PUBLIC_KEYS_PATH", default_public_keys_path)
   Dir.glob(File.join(public_keys_path, "*.pub")).each {|public_key| GPGME::Key.import(public_key)}
 end
