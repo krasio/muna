18a9265272fe190e6bc8acbf344b3585aaf8d47c
Krasimir Angelov <krasio@codingspree.net>
2018-10-18 14:57:02 +1300

    Implement copy to clipboard.

--- app/assets/javascripts/secrets.js
+++ app/assets/javascripts/secrets.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+window.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("#copy-to-clipboard").addEventListener("click", e => {
+    const secret = document.getElementById("encrypted-secret");
+    window.getSelection().selectAllChildren(secret);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+  });
+});
