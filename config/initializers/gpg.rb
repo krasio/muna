c4cfa271319fc02409237c185114fb08186fab02
Krasimir Angelov <krasio@codingspree.net>
2018-10-18 15:34:57 +1300

    Implement support for injecting public keys path as ENV var.

--- config/initializers/gpg.rb
+++ config/initializers/gpg.rb
@@ -1,4 +1,5 @@
-GPG_RECIPIENTS = Dir.glob(File.join(Rails.root, "config/gpg", "*.pub")).map do |public_key|
+public_keys_path = ENV.fetch("GPG_PUBLIC_KEYS", File.join(Rails.root, "config/gpg", "*.pub"))
+GPG_RECIPIENTS = Dir.glob(public_keys_path).map do |public_key|
   GPGME::Key.import(public_key)
   File.basename(public_key, ".pub").tap {|r| Rails.logger.info "Imported public key for #{r}."}
 end
