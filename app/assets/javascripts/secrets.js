5489e7012977e5a0f9876d8a9476ac87a816d2c3
Krasimir Angelov <krasio@codingspree.net>
2018-10-18 17:01:16 +1300

    Fix - attach events only if Copy and Download buttons exists.

--- app/assets/javascripts/secrets.js
+++ app/assets/javascripts/secrets.js
@@ -2,20 +2,24 @@
 // All this logic will automatically be available in application.js.
 
 window.addEventListener("DOMContentLoaded", function() {
-  document.querySelector("#copy-to-clipboard").addEventListener("click", e => {
-    const secret = document.getElementById("encrypted-secret");
-    const selection = window.getSelection();
-    selection.selectAllChildren(secret);
-    document.execCommand("copy");
-    setTimeout(function(s) { s.removeAllRanges(); }, 500, selection);
-  });
+  if (copy = document.querySelector("#copy-to-clipboard")) {
+    copy.addEventListener("click", e => {
+      const secret = document.getElementById("encrypted-secret");
+      const selection = window.getSelection();
+      selection.selectAllChildren(secret);
+      document.execCommand("copy");
+      setTimeout(function(s) { s.removeAllRanges(); }, 500, selection);
+    });
+  }
 
-  document.querySelector("#file-download").addEventListener("click", e => {
-    const secret = document.getElementById("encrypted-secret");
-    const file = document.createElement('a');
-    file.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(secret.innerText);
-    file.target = '_blank';
-    file.download = secret.dataset.filename + '.gpg';
-    file.click();
-  });
+  if (download = document.querySelector("#file-download")) {
+    download.addEventListener("click", e => {
+      const secret = document.getElementById("encrypted-secret");
+      const file = document.createElement('a');
+      file.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(secret.innerText);
+      file.target = '_blank';
+      file.download = secret.dataset.filename + '.gpg';
+      file.click();
+    });
+  }
 });
