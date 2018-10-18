eab483d86a3b331aff3f9a248902f125b6463c6f
Krasimir Angelov <krasio@codingspree.net>
2018-10-18 15:30:32 +1300

    Implement random subtitles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def subtitles
+    [
+      "(verb) (-ia) to talk privately.",
+      "(stative) be confidential, anonymous, secretly.",
+      "(noun) secret, confidentiality, secrecy."
+    ]
+  end
 end
